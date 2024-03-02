d533bf84047f73aa0ce20eb05e7850f1823b2e3e
Sashika Nawarathne <hi@sashika.me>
2024-03-02 17:32:08 +0000

    fix: expose origin for server actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "f9f1cb89-55ac-4414-bda0-fb17e15f9352.e1-eu-north-azure.choreoapps.dev",
+      ],
+    },
+  },
+};
 
 export default nextConfig;
